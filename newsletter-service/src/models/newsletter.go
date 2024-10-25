a328c9d66f6070b53d112a288b9125c4a9c7b233
Jorge Ocaris <jorgeocarisaa@gmail.com>
2024-10-25 12:43:54 -0400

    fix/adding non important secrets

--- newsletter-service/src/models/newsletter.go
+++ newsletter-service/src/models/newsletter.go
@@ -7,13 +7,13 @@ type Newsletter struct {
 	Title      string    `gorm:"default:'Newsletter'" json:"title"`
 	Attachment string    `json:"attachment"`
 	Html       string    `gorm:"type:text" json:"html"`
-	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	CreatedAt  time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
 
-	ScheduledDate *time.Time `gorm:"type:date" json:"scheduled_date"`
+	ScheduledDate *time.Time `gorm:"type:date" json:"scheduledDate"`
 	Scheduled     bool       `gorm:"default:false" json:"scheduled"`
 
-	CategoryID *uint     `gorm:"index" json:"category_id,omitempty"`
+	CategoryID *uint     `gorm:"index" json:"categoryId,omitempty"`
 	Category   *Category `gorm:"foreignKey:CategoryID" json:"category,omitempty"`
 
 	Recipients []Recipient `gorm:"many2many:newsletter_recipients;" json:"recipients,omitempty"`
@@ -31,7 +31,7 @@ type Recipient struct {
 	ID            string       `gorm:"primaryKey" json:"id"`
 	Email         string       `gorm:"unique" json:"email"`
 	UnsuscribeUrl string       `gorm:"type:varchar(255)" json:"unsuscribeUrl"`
-	CreatedAt     time.Time    `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt     time.Time    `gorm:"autoUpdateTime" json:"updated_at"`
+	CreatedAt     time.Time    `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt     time.Time    `gorm:"autoUpdateTime" json:"updatedAt"`
 	Newsletters   []Newsletter `gorm:"many2many:newsletter_recipients;" json:"newsletters,omitempty"`
 }
